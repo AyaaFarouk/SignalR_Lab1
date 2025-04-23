baf8d4aa43fc14032eb74632de56b6dfba08d8bb
AyaaFarouk <140838704+AyaaFarouk@users.noreply.github.com>
2025-04-23 23:34:58 +0200

    Push Lab

---
+++ Lab1/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using Lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab1.Controllers
+{
+    public class CourseController : Controller
+    {
+        public ITIContext _context { get; set; }
+        public CourseController(ITIContext context) 
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //----------------------------------------
+        public IActionResult DisplayCourse()
+        {
+            return View(_context.Courses.ToList());
+        }
+
+    }
+}
---
+++ Lab1/Hubs/CourseHub.cs
@@ -0,0 +1,24 @@
+﻿using Lab1.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Lab1.Hubs
+{
+    public class CourseHub : Hub
+    {
+        public ITIContext _context { get; set; }
+        public CourseHub(ITIContext context)
+        {
+            _context = context;
+        }
+        public void addcourse(Course course)
+        {
+            //save in db
+            _context.Add(course);
+            _context.SaveChanges();
+
+            //send data to all online client
+            Clients.All.SendAsync("newcourse", course.Crs_Id, course.Crs_Name, course.Crs_Duration, course.Top_Id);
+        }
+
+    }
+}
